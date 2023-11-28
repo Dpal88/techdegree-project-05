bc0bf79980411e61a4c283194a9f1078233ce66a
Dominic Palomino <MynameisDomPal@gmail.com>
2023-11-27 18:51:34 -0600

    Fixed JS for search filter

--- photo_gallery_v5/JS/app.js
+++ photo_gallery_v5/JS/app.js
@@ -4,18 +4,25 @@
 // Added an event listener to 'searchBar' that listens for the keyup event,
 // and stores in the event object called 'e'.
 // Then inside the function, is a variable called 'currentValue' which tracks the value in the search bar.
+// 'anchor' variable stores the a tag element from index.html
+// 'anchorArray' creates an array from the 'anchor' variable (a tag)
 
 const searchBar = document.getElementById('searchBar');
 searchBar.addEventListener('keyup', e => {
     let currentValue = e.target.value.toLowerCase();
-    const anchor = document.getElementsByClassName('anchor');
-    let captions = anchor.getAttribute('data-caption');
+    console.log(currentValue);
+    let anchor = document.getElementsByClassName('anchor');
+    console.log(anchor);
+    const anchorArray = Array.from(anchor);
+    console.log(anchorArray);
     
-    captions.forEach(caption =>{
-        if(caption.textContent.toLowerCase().includes(currentValue)) {
-            caption.style.display = 'block';
+    anchorArray.forEach(a =>{
+        let captions = a.getAttribute('data-caption');
+        console.log(captions);
+        if(captions.toLowerCase().includes(currentValue)) {    
+            a.style.display = '';
         } else {
-            caption.style.display = 'none';
+            a.style.display = 'none';
         }
     });
 });
--- photo_gallery_v5/JS/search.js
+++
@@ -1,8 +0,0 @@
-// Accesses the input element in the HTML document & 
-document.getElementsByClassName('anchor').addEventListener("key up", inputFeild)
-// 
-function inputFeild() {
-    let x = document.getElementsByClassName('anchor');
-    x.value = x.value.toLowerCase();
-}
-//
